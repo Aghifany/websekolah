c95cfa0d264e6166a2f0ab3a38f0f2c3e14db233
Aghifany <aghifany8@gmail.com>
2022-04-25 14:42:04 +0700

    update

--- resources/views/_layout/_header.blade.php
+++ resources/views/_layout/_header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
   <div class="container">
     <a class="navbar-brand" href="assets/img/apple-touch-icon.png">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,9 +37,9 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
   <div class="container">
- <a class="navbar-brand" href="#"><img src="assets/smk.png"/></a>
+ <a class="navbar-brand" href="#"><img src="assets/smk2.png"/></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
